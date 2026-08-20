fa05fa9fdc9b5d0594b91d6794d17139568937da
Amit khichar <142559591+amitkhichar01@users.noreply.github.com>
2026-08-20 22:14:48 +0530

    added frontend production domain name in cors middleware

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use(
       "http://localhost:3001",
       "http://localhost:5173",
       "http://localhost:5174",
+      "https://meknos.amitkhichar.in"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
